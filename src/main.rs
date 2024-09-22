aa56e582e1decab45d142cf5682d0e35e799b598
clemlak <clementlakhal@gmail.com>
2024-09-22 13:15:41 +0400

    add some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::io;
+
+fn main()  -> io::Result<()> {
+    let file_content = fs::read_to_string("./foo.huff")?;
+    println!("{}", file_content);
+    Ok(())
 }
